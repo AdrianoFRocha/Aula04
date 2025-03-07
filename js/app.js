8a46f8ae24f5d203a5dccbb2694610f77898fe3b
AdrianoFRocha <151871187+AdrianoFRocha@users.noreply.github.com>
2025-03-07 12:30:02 -0300

    [find] app js

--- js/app.js
+++ js/app.js
@@ -48,7 +48,49 @@ let feira = frutas.concat(legumes)
 
 console.log(feira)
 
+// cCopiando uma parte do vetor
 let frutas_sel = frutas.slice(1,4)
 
 console.log(frutas_sel)
 
+let frutas_c = frutas.slice(1,1,"Manga")
+console.log(frutas_c)
+
+//locaLizar elementos
+let situacao = frutas.includes("Manga")
+console.log(situacao)
+
+situacao = frutas.includes("caju")
+console.log(situacao)
+
+let texto = frutas.join( ' - ')
+console.log(texto)
+
+//Função map
+let numeros1 = [1,2,3,4,5,6]
+let dobra = numeros1.map(x=>x*2)
+
+console.log(dobra)
+
+// Função Filter
+let pares = numeros1.filter(x => x %2 == 0)
+console.log(pares)
+
+let loc = numeros1.find(x=> x>2)
+
+console.log(loc)
+
+let locend = numeros1.findIndex(x=> x>2)
+
+console.log(locend)
+
+let maior = numeros1.every(x=> x>6)
+
+console.log(maior)
+
+
+let numeros2 = [3,5,4,1,6,9,7,0,2]
+
+console.log(numeros2.sort())
+
+console.log(numeros2.reverse())
